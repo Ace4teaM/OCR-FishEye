28bbbee22417d6d0a58e97240f3f27dd431fee75
Thomas Auguey <thomas.auguey@hotmail.com>
2026-04-24 15:43:59 +0200

    Fix la marge de l'en-tete

--- fish-eye/app/page.js
+++ fish-eye/app/page.js
@@ -1,6 +1,5 @@
 import styles from "./page.module.css";
 import { prisma } from "@/lib/prisma.js";
-import DropMenu from "@/components/DropMenu/DropMenu.jsx";
 import CardGrid from "@/components/CardGrid/CardGrid.jsx";
 import Banner from "@/components/Banner/Banner.jsx";
 
@@ -14,9 +13,6 @@ export default async function Home() {
       <header>
         <Banner title = "Nos photographes"></Banner>
       </header>
-      <nav>
-        <DropMenu></DropMenu>
-      </nav>
       <main>
         <CardGrid items={all}></CardGrid>
       </main>
