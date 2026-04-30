70a7f444f1012a8d0609f14fd4a5a21c9b8dcae4
Thomas Auguey <thomas.auguey@hotmail.com>
2026-04-30 16:51:42 +0200

    Rend le logo accessible au clavier

--- fish-eye/components/Banner/Banner.jsx
+++ fish-eye/components/Banner/Banner.jsx
@@ -15,7 +15,7 @@ const Banner = ({
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <Link href="/" aria-label="Retour à l'accueil">
+        <Link tabIndex={0} href="/" aria-label="Retour à l'accueil">
           <img src="/logo.png" height={40} alt="logo"></img>
         </Link>
         {title && <h1>{title}</h1>}
