95308aaf75e08285b65876473a6c90f9f723430f
Thomas Auguey <thomas.auguey@hotmail.com>
2026-05-04 11:53:15 +0200

    fix déjà le role de lien par semantique

--- fish-eye/components/Banner/Banner.jsx
+++ fish-eye/components/Banner/Banner.jsx
@@ -16,7 +16,7 @@ const Banner = ({
     <div className={styles.container}>
       <div className={styles.content}>
         <Link tabIndex={0} href="/" aria-label="Retour à l'accueil">
-          <img src="/logo.png" height={40} alt="logo"></img>
+          <img src="/logo.png" height={40} alt=""></img>
         </Link>
         {title && <h1>{title}</h1>}
       </div>
